e7596235116f002835612103fdbee31a9d4797b2
ysgorin <ysgorin@gmail.com>
2023-08-07 13:29:02 -0400

    completed script for print_sep_end.py

---
+++ print_sep_end.py
@@ -0,0 +1,6 @@
+# Modify the following code, so that it reads "Programming***Essentials***in...Python"
+# print("Programming","Essentials","in")
+# print("Python")
+
+print("Programming","Essentials","in", sep="***", end="...")
+print("Python")
