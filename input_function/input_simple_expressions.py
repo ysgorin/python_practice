b04f97b1cfe29b7bd76887991ace7969a065c7b5
ysgorin <ysgorin@gmail.com>
2023-08-07 16:26:43 -0400

    added input_simple_expressions.py

---
+++ input_function/input_simple_expressions.py
@@ -0,0 +1,22 @@
+# Your task is to complete the code in order to
+# evaluate the results of four basic arithmetic operations.
+# The results have to be printed to the console.
+# You may not be able to protect the code from
+# a user who wants to divide by zero.
+# That's okay, don't worry about it for now.
+
+# Starter Code:
+# input a float value for variable a here
+# input a float value for variable b here
+
+# output the result of addition here
+# output the result of subtraction here
+# output the result of multiplication here
+# output the result of division here
+
+# print("\nThat's all, folks!")
+
+a,b = float(input('Enter a number: ')), float(input('Enter a number: '))
+
+print('a + b =',(a+b),'\na - b =',(a-b),'\n\
+a * b =',(a*b),'\na / b =',(a/b))
