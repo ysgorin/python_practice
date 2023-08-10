96dbef1792112afc3f95b9691d958c2fe2422d7d
ysgorin <ysgorin@gmail.com>
2023-08-10 11:17:06 -0400

    added comparison_practice.py

---
+++ comparisons_and_conditionals/comparison_practice.py
@@ -0,0 +1,10 @@
+# Using one of the comparison operators in Python,
+# write a simple two-line program that takes the
+# parameter n as input, which is an integer,
+# and prints False if n is less than 100,
+# and True if n is greater than or equal to 100.
+
+# Don't create any if blocks
+
+n = int(input("Enter a number: "))
+print(n >= 100)
