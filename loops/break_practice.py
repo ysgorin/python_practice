42b63029c0e209dd6155dfa5162078cb261aa804
ysgorin <ysgorin@gmail.com>
2023-08-11 10:52:53 -0400

    added break_practice.py

---
+++ loops/break_practice.py
@@ -0,0 +1,16 @@
+# The break statement is used to exit/terminate a loop.
+
+# Design a program that uses a while loop and
+# continuously asks the user to enter a word unless the user
+# enters "chupacabra" as the secret exit word,
+# in which case the message "You've successfully left the loop."
+# should be printed to the screen, and the loop should terminate.
+
+# Don't print any of the words entered by the user.
+# Use the concept of conditional execution and the break statement.
+
+while True:
+    message = input('Enter a word: ')
+    if message == 'chupacabra':
+        break
+print('You\'ve successfully left the loop.')
