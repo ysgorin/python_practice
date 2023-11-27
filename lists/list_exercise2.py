1622458d0c53a57e9b26e605391b712314e3e6fe
ysgorin <ysgorin@gmail.com>
2023-11-27 09:08:26 -0500

    finished code

--- lists/list_exercise2.py
+++ lists/list_exercise2.py
@@ -13,44 +13,57 @@
 # and Richard Starkey (better known as Ringo
 # Starr).
 
-
 # Write a program that reflects these changes and
 # lets you practice with the concept of lists.
 # Your task is to:
+
+# step 5: use the insert() method to add Ringo
+# Starr to the beginning of the list.
+
+# Dependencies
+import time
+
+# step 1: create an empty list named beatles;
+# step 1
+beatles = []
+print("Step 1:", beatles)
+time.sleep(1)
 
 # step 2: use the append() method to add the
 # following members of the band to the list:
 # John Lennon, Paul McCartney, and George
 # Harrison;
+# step 2
+beatles.append('John Lennon')
+beatles.append('Paul McCartney')
+beatles.append('George Harrison')
+print("Step 2:", beatles)
+time.sleep(1)
 
 # step 3: use the for loop and the append()
 # method to prompt the user to add the following
 # members of the band to the list: Stu Sutcliffe,
 # and Pete Best;
+# step 3
+user_input = [input('Type \'Stu Sutcliffe\' to add him to the band. '), input('Type \'Pete Best\' to add him to the band. ')]
+for input in user_input:
+    beatles.append(input)
+print("Step 3:", beatles)
+time.sleep(1)
 
 # step 4: use the del instruction to remove Stu
 # Sutcliffe and Pete Best from the list;
+# step 4
+del beatles[3:5]
+print("Step 4:", beatles)
+time.sleep(1)
 
 # step 5: use the insert() method to add Ringo
 # Starr to the beginning of the list.
-
-# step 1: create an empty list named beatles;
-# step 1
-
-beatles = []
-print("Step 1:", beatles)
-
-# step 2
-print("Step 2:", beatles)
-
-# step 3
-print("Step 3:", beatles)
-
-# step 4
-print("Step 4:", beatles)
-
 # step 5
+beatles.insert(0, 'Ringo Starr')
 print("Step 5:", beatles)
+time.sleep(1)
 
 # testing list length
 print("The Fab", len(beatles))
