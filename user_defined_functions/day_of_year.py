441f181426fcda28c41c6edc0823551b043729aa
ysgorin <ysgorin@gmail.com>
2024-02-08 13:25:46 -0500

    added file day_of_year.py

---
+++ user_defined_functions/day_of_year.py
@@ -0,0 +1,22 @@
+# Your task is to write and test a function which
+# takes three arguments (a year, a month, and a
+# day of the month) and returns the corresponding
+#  day of the year, or returns None if any of the
+# arguments is invalid.
+
+def is_year_leap(year):
+#
+# Your code from LAB 4.3.1.6.
+#
+
+def days_in_month(year, month):
+#
+# Your code from LAB 4.3.1.7.
+#
+
+def day_of_year(year, month, day):
+#
+# Write your new code here.
+#
+
+print(day_of_year(2000, 12, 31))
