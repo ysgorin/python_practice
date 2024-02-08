8dfd0568b5430006aaca732512c18335ca3036ec
ysgorin <ysgorin@gmail.com>
2024-02-08 14:36:58 -0500

    added previously written functions

--- user_defined_functions/day_of_year.py
+++ user_defined_functions/day_of_year.py
@@ -5,18 +5,35 @@
 # arguments is invalid.
 
 def is_year_leap(year):
-#
-# Your code from LAB 4.3.1.6.
-#
-
+    if(year % 4 != 0):
+        return False
+    elif(year % 100 != 0):
+        return True
+    elif(year % 400 != 0):
+        return False
+    else:
+        return True
+    
 def days_in_month(year, month):
-#
-# Your code from LAB 4.3.1.7.
-#
+    month_lengths = {1: 31,
+                     3: 31,
+                     4: 30,
+                     5: 31,
+                     6: 30,
+                     7: 31,
+                     8: 31,
+                     9: 30,
+                     10: 31,
+                     11: 30,
+                     12: 31}
+    if month == 2 and is_year_leap(year) == False:
+        return 28
+    elif month == 2 and is_year_leap(year) == True:
+        return 29
+    else:
+        return month_lengths[month]
 
 def day_of_year(year, month, day):
-#
-# Write your new code here.
-#
+    year
 
 print(day_of_year(2000, 12, 31))
