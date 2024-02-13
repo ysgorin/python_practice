6522ecc5f8ca6bbfcd1eb9a79dbf90b33ea58591
ysgorin <ysgorin@gmail.com>
2024-02-13 10:54:43 -0500

    completed code for prime_numbers.py

--- user_defined_functions/prime_numbers.py
+++ user_defined_functions/prime_numbers.py
@@ -2,14 +2,6 @@
 # A natural number is prime if it is greater than
 # 1 and has no divisors other than 1 and itself.
 
-# Complicated? Not at all. For example, 8 isn't a
-# prime number, as you can divide it by 2 and 4
-# (we can't use divisors equal to 1 and 8, as the
-# definition prohibits this).
-
-# On the other hand, 7 is a prime number, as we
-# can't find any legal divisors for it.
-
 # Your task is to write a function checking
 # whether a number is prime or not.
 
@@ -32,9 +24,11 @@
 # x0.5
 
 def is_prime(num):
-#
-# Write your code here.
-#
+    for i in range(2,num):
+        if num % i == 0:
+           return False
+    else:
+        return True
 
 for i in range(1, 20):
 	if is_prime(i + 1):
