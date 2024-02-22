9b00123fc48baaf43a6db7ec324efc3e4ba9a4a3
ysgorin <ysgorin@gmail.com>
2024-02-22 10:24:49 -0500

    added bmi_calc3.py file

---
+++ two_param_functions/bmi_calc3.py
@@ -0,0 +1,17 @@
+# Function to evaluate Body Mass Index (BMI
+# Weight (kg) / height (m)^2
+
+def bmi(weight, height):
+    if height < 1.0 or height > 2.5 or \
+    weight < 20 or weight > 200:
+        return None
+    
+    return weight / height ** 2
+
+weight_input = \
+float(input('What is your weight (kg)? '))
+
+height_input = \
+float(input('What is your height? (m) '))
+
+print(bmi(weight_input, height_input))
