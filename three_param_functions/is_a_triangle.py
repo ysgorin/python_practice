34b931bcec5136a85cea26d829bd26a2f3b6ccad
ysgorin <ysgorin@gmail.com>
2024-02-27 11:22:11 -0500

    edited code to include more test prompts

--- three_param_functions/is_a_triangle.py
+++ three_param_functions/is_a_triangle.py
@@ -31,5 +31,23 @@
 def is_a_triangle(a, b, c):
     return a + b > c and b + c > a and c + a > b
 
-print(is_a_triangle(1, 1, 1))
-print(is_a_triangle(1, 1, 3))
+# Used for testing
+# print(is_a_triangle(1, 1, 1))
+# print(is_a_triangle(1, 1, 3))
+
+print('Follow the prompts to check if your three\
+ side lengths can be a triangle.')
+
+a = float(input('Enter the first side\'s length:\
+ '))
+
+b = float(input('Enter the second side\'s\
+length: '))
+
+c = float(input('Enter the third side\'s length:\
+ '))
+
+if is_a_triangle(a, b, c):
+    print('Yes, it can be a triangle.')
+else:
+    print('No, it can\'t be a triangle.')
