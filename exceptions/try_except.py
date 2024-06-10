90d2460e39db7064d03d93ee8f3d987dfe621a7c
ysgorin <ysgorin@gmail.com>
2024-06-10 11:23:38 -0400

    added more code to try_except.py

--- exceptions/try_except.py
+++ exceptions/try_except.py
@@ -1,2 +1,34 @@
-value = int(input('Enter a natural number: '))
-print('The reciprocal of', value, 'is', 1/value)
+# The following code will raise an error if a
+# non-integer or nothing is inputted.
+# value = int(input('Enter a natural number: '))
+# print('The reciprocal of', value, 'is', 1/value)
+
+# A try-except branch can handle the error.
+# try:
+#     value = int(input('Enter a natural number: '))
+#     print('The reciprocal of', value, 'is', 1/value)
+# except:
+#     print('I don't know what to do.')
+
+# With the above, all errors will be handled equally.
+# The following distinguishes between errors.
+# try:
+#     value = int(input('Enter a natural number: '))
+#     print('The reciprocal of', value, 'is', 1/value)
+# except ValueError:
+#     print('I don\'t know what to do.')
+# except ZeroDivisionError:
+#     print('Division by zero is not allowed in our Universe.')
+
+
+# While all exceptions are covered in the above code,
+# this code has a default except branch as well.
+try:
+    value = int(input('Enter a natural number: '))
+    print('The reciprocal of', value, 'is', 1/value)
+except ValueError:
+    print('I don\'t know what to do.')
+except ZeroDivisionError:
+    print('Division by zero is not allowed in our Universe.')
+except:
+    print('Something strange has happened here... Sorry!')
