1828e61d86fa7c76097170ddecd577936aeec540
ysgorin <ysgorin@gmail.com>
2024-06-24 12:22:35 -0400

    continued work on tic_tac_toe.py

--- projects/tic_tac_toe.py
+++ projects/tic_tac_toe.py
@@ -15,27 +15,36 @@ def display_board(board):
 |       |       |       |\n\
 +-------+-------+-------+")
 
-board = [[1,2,3],[4,"X",6],[7,8,9]]
-
-display_board(board)
+def make_list_of_free_fields(board):
+#     The function browses the board and builds a list of all the free squares; 
+#     the list consists of tuples, while each tuple is a pair of row and column numbers.
+    free_fields = []
+    for row in board:
+        for column in row:
+            if type(column) == int:
+                if column in [1,2,3]:
+                    free_fields.append((1,column))
+                elif column in [4,5,6]:
+                    free_fields.append((2,column))
+                elif column in [7,8,9]:
+                    free_fields.append((3,column))
+    return free_fields
 
 # def enter_move(board):
     # The function accepts the board's current status, asks the user about their move, 
     # checks the input, and updates the board according to the user's decision.
 
-
-# def make_list_of_free_fields(board):
-    # The function browses the board and builds a list of all the free squares; 
-    # the list consists of tuples, while each tuple is a pair of row and column numbers.
-
-
 # def victory_for(board, sign):
-    # The function analyzes the board's status in order to check if 
-    # the player using 'O's or 'X's has won the game
-
+#     The function analyzes the board's status in order to check if 
+#     the player using 'O's or 'X's has won the game
 
 # def draw_move(board):
-    # The function draws the computer's move and updates the board.
+#     The function draws the computer's move and updates the board.
+
+board = [[1,2,3],[4,"X",6],[7,8,9]]
+
+display_board(board)
+print(make_list_of_free_fields(board))
 
 # Your task is to write a simple program which
 # pretends to play tic-tac-toe with the user. To
