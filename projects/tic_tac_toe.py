4ec115584a6a76738808a322d14325982ecaa498
ysgorin <ysgorin@gmail.com>
2024-06-25 11:26:13 -0400

    made adjustments to tic_tac_toe.py

--- projects/tic_tac_toe.py
+++ projects/tic_tac_toe.py
@@ -1,4 +1,5 @@
 from random import randrange
+from time import sleep
 
 def display_board(board):
     # The function accepts one parameter containing the board's current status
@@ -30,6 +31,8 @@ def enter_move(board):
             valid_moves.append(3)
         elif field == (2,1):
             valid_moves.append(4)
+        elif field == (2,2):
+            valid_moves.append(5)
         elif field == (2,3):
             valid_moves.append(6)
         elif field == (3,1):
@@ -95,6 +98,8 @@ def draw_move(board):
             valid_moves.append(3)
         elif field == (2,1):
             valid_moves.append(4)
+        elif field == (2,2):
+            valid_moves.append(5)
         elif field == (2,3):
             valid_moves.append(6)
         elif field == (3,1):
@@ -120,9 +125,13 @@ def tie_game(board):
                 return False
     return True    
 
-board = [[1,2,3],[4,"X",6],[7,8,9]]
-free_fields = [(1, 1), (1, 2), (1, 3), (2, 1), (2, 3), (3, 1), (3, 2), (3, 3)]
+print("Let's play Tic Tac Toe.\n\
+You are 'O' and the computer is 'X'\n\
+To move, choose the number of a free space.")
+
+board = [[1,2,3],[4,5,6],[7,8,9]]
 display_board(board)
+sleep(1)
 
 while True:
     enter_move(board)
@@ -133,6 +142,7 @@ while True:
     elif tie_game(board) == True:
         print("Tie game.")
         break
+    sleep(1)
     draw_move(board)
     display_board(board)
     if victory_for(board, "X") == True:
@@ -141,53 +151,4 @@ while True:
     elif tie_game(board) == True:
         print("Tie game.")
         break
-
-# Your task is to write a simple program which
-# pretends to play tic-tac-toe with the user. To
-# make it all easier for you, we've decided to
-# simplify the game. Here are our assumptions:
-# * the computer (i.e., your program) should play
-# the game using 'X's;
-# * the user (e.g., you) should play the game
-# using 'O's;
-# * the first move belongs to the computer − it
-# always puts its first 'X' in the middle of the
-# board;
-# * all the squares are numbered row by row
-# starting with 1 (see the example session below
-# for reference)
-# * the user inputs their move by entering the
-# number of the square they choose − the number
-# must be valid, i.e., it must be an integer, it
-# must be greater than 0 and less than 10, and it
-# cannot point to a field which is already
-# occupied;
-# * the program checks if the game is over −
-# there are four possible verdicts: the game
-# should continue, the game ends with a tie, you
-# win, or the computer wins;
-# * the computer responds with its move and the
-# check is repeated;
-# * don't implement any form of artificial
-# intelligence − a random field choice made by
-# the computer is good enough for the game.
-
-# Implement the following features:
-# * the board should be stored as a three-element
-# list, while each element is another
-# three-element list (the inner lists represent
-# rows) so that all of the squares may be
-# accessed using the following syntax:
-# board[row][column]
-# * each of the inner list's elements can contain
-# 'O', 'X', or a digit representing the square's
-# number (such a square is considered free)
-# * the board's appearance should be exactly the
-# same as the one presented in the example.
-# * implement the functions defined for you in
-# the editor.
-# * Drawing a random integer number can be done
-# by utilizing a Python function called
-# randrange(). The example program below shows
-# how to use it (the program prints ten random
-# numbers from 0 to 8).
+    sleep(1)
