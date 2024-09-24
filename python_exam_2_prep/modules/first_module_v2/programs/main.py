a82ac1ad991d16716e8803c9b25b3b136238735b
ysgorin <ysgorin@gmail.com>
2024-09-24 15:32:38 -0400

    changed directory structure, added sys.path.append to main.py

---
+++ python_exam_2_prep/modules/first_module_v2/programs/main.py
@@ -0,0 +1,11 @@
+import sys
+
+sys.path.append('..\\modules')
+
+import module
+
+zeroes = [0 for i in range(5)]
+ones = [1 for i in range(5)]
+
+print(module.sum1(zeroes))
+print(module.prod1(ones))
