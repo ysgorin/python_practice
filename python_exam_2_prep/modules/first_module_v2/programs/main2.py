598f205829eaff4cb72cc1d46b68160720faddb2
ysgorin <ysgorin@gmail.com>
2024-09-27 14:21:01 -0400

    added function imports from modules nested in package directories

--- python_exam_2_prep/modules/first_module_v2/programs/main2.py
+++ python_exam_2_prep/modules/first_module_v2/programs/main2.py
@@ -6,3 +6,17 @@ path.append('..\\packages')
 # Add type: ignore to skip problem detection for that line.
 import extra.iota # type: ignore
 print(extra.iota.FunI())
+
+# import extra.good.best.sigma, extra.good.best.tau # type: ignore
+# print(extra.good.best.sigma.FunS())
+# print(extra.good.best.tau.FunT())
+
+# from extra.good.best.sigma import FunS # type: ignore
+# from extra.good.best.tau import FunT # type: ignore
+# print(FunS())
+# print(FunT())
+
+import extra.good.best.sigma as sig # type: ignore
+import extra.good.best.tau as tau # type: ignore
+print(sig.FunS())
+print(tau.FunT())
