e8c1a57fc4bee8ec2a1f8721e76911e641ba7500
ysgorin <ysgorin@gmail.com>
2024-11-05 16:06:45 -0500

    cleaned code

--- python_exam_2_prep/modules/second_module/number_processor.py
+++ python_exam_2_prep/modules/second_module/number_processor.py
@@ -2,13 +2,13 @@
 
 line = input("Enter a line of numbers - separate them with spaces: ")
 strings = line.split()
-if len(strings) == 0:
-    total = 'You didn\'t enter any numbers.'
-else:
-    total = 0
+total = 0
 try:
     for substr in strings:
         total += float(substr)
-    print("The total is:", total)
+    if len(strings) == 0:
+        print("The total is:", total)
+    else:
+        print('You didn\'t enter any numbers.')
 except:
     print(substr, "is not a number.")
