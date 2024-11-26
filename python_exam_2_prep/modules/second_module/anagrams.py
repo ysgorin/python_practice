215b8a887ffd68af0ebe31988701a0ae8581fdc0
ysgorin <ysgorin@gmail.com>
2024-11-26 11:16:26 -0500

    added background and instruction comments

---
+++ python_exam_2_prep/modules/second_module/anagrams.py
@@ -0,0 +1,27 @@
+# anagrams.py
+
+# An anagram is a new word formed by rearranging
+# the letters of a word, using all the original
+# letters exactly once.
+
+# Write a program that asks the user for two
+# separate texts, checks whether the entered
+# texts are anagrams, and prints the result.
+
+# Note:
+# Assume that two empty strings are not anagrams
+# Treat upper- and lower-case letters as equal
+# Spaces are not taken into account during the check - treat them as non-existent
+
+# Test Data
+# Sample Input:
+#    'Listen'
+#    'Silent'
+# Sample Output:
+#    'Anagrams'
+
+# Sample Input:
+#    'modern'
+#    'norman'
+# Sample Output:
+#    'Not anagrams'
