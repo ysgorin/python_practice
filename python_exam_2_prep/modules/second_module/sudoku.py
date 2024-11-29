fb73eff380766ff759dd39c5d111fb01c244e8b5
ysgorin <ysgorin@gmail.com>
2024-11-29 09:44:10 -0500

    continue coding

--- python_exam_2_prep/modules/second_module/sudoku.py
+++ python_exam_2_prep/modules/second_module/sudoku.py
@@ -12,4 +12,14 @@ sudoku_dict = {}
 for i in range(1,10):
     read_row(sudoku_dict, i)
 
+validate_list = [1,2,3,4,5,6,7,8,9]
+
+# Check Horizontal
+for i in range(1,10):
+    line_list = list(sudoku_dict[i]).sort()
+    if line_list == validate_list:
+        continue
+    else:
+        break
+
 print(sudoku_dict)
