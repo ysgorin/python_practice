aecebd887c96b3883ca5e0db0dba051d7a8a0a94
ysgorin <ysgorin@gmail.com>
2024-11-30 22:56:25 -0500

    tested in jupyter and completed code

--- python_exam_2_prep/modules/second_module/sudoku.py
+++ python_exam_2_prep/modules/second_module/sudoku.py
@@ -8,11 +8,11 @@ def read_row(sudoku_dict, row_num):
             print('Invalid input. Enter nine digits using numbers 1-9.')
 
 def check_grid(sudoku_dict, row_num, col_num, validate_list):
+    grid_list = []
     for row in range(3):
         for col in range(3):
-            grid_list = []
             grid_list.append(sudoku_dict[row_num+row][col_num+col])
-    if grid_list.sort() == validate_list:
+    if sorted(grid_list) == validate_list:
         return True
     else:
         return False
@@ -22,37 +22,37 @@ sudoku_dict = {}
 for i in range(1,10):
     read_row(sudoku_dict, i)
 
-validate_list = [1,2,3,4,5,6,7,8,9]
+validate_list = ['1','2','3','4','5','6','7','8','9']
 
 validate_flag = True
-# while validate_flag:
-#     # Check Horizontal
-#     for i in range(1,10):
-#         line_list = list(sudoku_dict[i]).sort()
-#         if line_list == validate_list:
-#             continue
-#         else:
-#             validate_flag = False
-#             break
-#     # Check Vertical
-#     for y in range(9):
-#         for i in range(1,10):
-#             line_list = []
-#             line_list.append(sudoku_dict[i][y])
-#         if line_list.sort() == validate_list:
-#             continue
-#         else:
-#             validate_flag = False
-#             break
+while validate_flag:
+    # Check Horizontal
+    for i in range(1,10):
+        if sorted(list(sudoku_dict[i])) == validate_list:
+            continue
+        else:
+            validate_flag = False
+            break
+
+    # Check Vertical
+    for y in range(9):
+        line_list = []
+        for i in range(1,10):
+            line_list.append(sudoku_dict[i][y])
+        if sorted(line_list) == validate_list:
+            continue
+        else:
+            validate_flag = False
+            break
     
-#     # # Check 3 x 3 grids
-#     # if check_grid(sudoku_dict, 1, 0, validate_list) \
-#     #     and check_grid(sudoku_dict, 4, 3, validate_list) \
-#     #         and check_grid(sudoku_dict, 7, 6, validate_list):
-#     #     break
-#     # else:
-#     #     validate_flag = False
-#     #     break
+    # Check 3 x 3 grids
+    if check_grid(sudoku_dict, 1, 0, validate_list) \
+        and check_grid(sudoku_dict, 4, 3, validate_list) \
+            and check_grid(sudoku_dict, 7, 6, validate_list):
+        break
+    else:
+        validate_flag = False
+        break
 
 if validate_flag:
     print('Yes')
