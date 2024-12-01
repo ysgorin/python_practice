ff25b916f7c27f9e27bcc66b6dc00150726e6e81
ysgorin <ysgorin@gmail.com>
2024-11-30 20:07:01 -0500

    added validation code

--- python_exam_2_prep/modules/second_module/sudoku.py
+++ python_exam_2_prep/modules/second_module/sudoku.py
@@ -7,6 +7,16 @@ def read_row(sudoku_dict, row_num):
         else:
             print('Invalid input. Enter nine digits using numbers 1-9.')
 
+def check_grid(sudoku_dict, row_num, col_num, validate_list):
+    for row in range(3):
+        for col in range(3):
+            grid_list = []
+            grid_list.append(sudoku_dict[row_num+row][col_num+col])
+    if grid_list == validate_list:
+        return True
+    else:
+        return False
+
 sudoku_dict = {}
 
 for i in range(1,10):
@@ -14,12 +24,37 @@ for i in range(1,10):
 
 validate_list = [1,2,3,4,5,6,7,8,9]
 
-# Check Horizontal
-for i in range(1,10):
-    line_list = list(sudoku_dict[i]).sort()
-    if line_list == validate_list:
-        continue
+validate_flag = True
+while validate_flag:
+    # Check Horizontal
+    for i in range(1,10):
+        line_list = list(sudoku_dict[i]).sort()
+        if line_list == validate_list:
+            continue
+        else:
+            validate_flag = False
+            break
+    # Check Vertical
+    for y in range(9):
+        for i in range(1,10):
+            line_list = []
+            line_list.append(sudoku_dict[i][y])
+        if line_list.sort() == validate_list:
+            continue
+        else:
+            validate_flag = False
+            break
+    
+    # Check 3 x 3 grids
+    if check_grid(sudoku_dict, 1, 0, validate_list) \
+        and check_grid(sudoku_dict, 4, 3, validate_list) \
+            and check_grid(sudoku_dict, 7, 6, validate_list):
+        break
     else:
+        validate_flag = False
         break
 
-print(sudoku_dict)
+if not validate_flag:
+    print('Yes')
+else:
+    print('No')
