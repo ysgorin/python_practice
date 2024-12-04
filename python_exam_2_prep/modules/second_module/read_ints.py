280e0b2bd03c50028299419b1cf33926e79afd95
ysgorin <ysgorin@gmail.com>
2024-12-04 10:45:46 -0500

    finished code

--- python_exam_2_prep/modules/second_module/read_ints.py
+++ python_exam_2_prep/modules/second_module/read_ints.py
@@ -5,7 +5,17 @@
 # range.
 
 def read_int(prompt, min, max):
-    return
+    while True:
+        try:
+            v = int(input(prompt))
+            if min <= v <= max:
+                return v
+            else:
+                raise Exception
+        except ValueError:
+            print('Error: wrong input')
+        except:
+            print(f'Error: the value is not within the permitted range ({min}..{max})')
 
 v = read_int("Enter a number from -10 to 10: ", -10, 10)
 
