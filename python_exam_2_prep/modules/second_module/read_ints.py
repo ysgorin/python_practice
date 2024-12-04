73b21fc4a71eb450a147a55482c80d2e9f57bb93
ysgorin <ysgorin@gmail.com>
2024-12-04 10:37:35 -0500

    added file

---
+++ python_exam_2_prep/modules/second_module/read_ints.py
@@ -0,0 +1,12 @@
+# read_ints.py
+
+# Write a function able to input intger values
+# and to check if they are within a specified
+# range.
+
+def read_int(prompt, min, max):
+    return
+
+v = read_int("Enter a number from -10 to 10: ", -10, 10)
+
+print("The number is:", v)
