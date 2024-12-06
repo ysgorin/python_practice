03c41e473a0a9c7f39d7be9571015546bc46b248
ysgorin <ysgorin@gmail.com>
2024-12-06 12:42:36 -0500

    completed object oriented approach code

--- python_exam_2_prep/modules/second_module/stack_object_approach.py
+++ python_exam_2_prep/modules/second_module/stack_object_approach.py
@@ -5,9 +5,26 @@ class Stack:
     # Define the constructor function.
     def __init__(self):
         # Add a list property
-        self.stack_list = []
+        # Use encapsulation '__' to make property
+        # private to the class
+        self.__stack_list = []
+
+    def push(self, val):
+        self.__stack_list.append(val)
+
+    def pop(self):
+        val = self.__stack_list[-1]
+        del self.__stack_list[-1]
+        return val
 
 # Instantiate the object.
 stack_object = Stack()
-# Access property using dot notation
-print(len(stack_object.stack_list))
+
+# Access properties using dot notation
+stack_object.push(3)
+stack_object.push(2)
+stack_object.push(1)
+
+print(stack_object.pop())
+print(stack_object.pop())
+print(stack_object.pop())
