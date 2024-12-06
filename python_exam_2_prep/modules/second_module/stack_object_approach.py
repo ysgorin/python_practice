1238a1591c58cd85e5f1325ac84bf7d2e151b20f
ysgorin <ysgorin@gmail.com>
2024-12-06 12:22:15 -0500

    started code

--- python_exam_2_prep/modules/second_module/stack_object_approach.py
+++ python_exam_2_prep/modules/second_module/stack_object_approach.py
@@ -1,1 +1,10 @@
 # stack_object_approach.py
+
+# Define the Stack class.
+class Stack:
+    # Define the constructor function.
+    def __init__(self):
+        print('Hi!')
+
+# Instantiate the object.
+stack_object = Stack()
