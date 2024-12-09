eedb17a3d04e873f085fb2adc55f7f9c8e638ce1
ysgorin <ysgorin@gmail.com>
2024-12-09 08:50:54 -0500

    completed code

--- python_exam_2_prep/third_module/queue_p1.py
+++ python_exam_2_prep/third_module/queue_p1.py
@@ -1,25 +1,57 @@
-class QueueError(???):  # Choose base class for the new exception.
-    #
-    #  Write code here
-    #
+# queue_p1.py
 
+# Scenario
+# As you already know, a stack is a data
+# structure realizing the LIFO (Last In – First
+# Out) model. It's easy and you've already grown
+# perfectly accustomed to it.
+
+# Let's try something new now. A queue is a data
+# model characterized by the term FIFO: First In
+# – First Out. Note: a regular queue (line) you
+# know from shops or post offices works exactly
+# in the same way – a customer who came first is
+# served first too.
+
+# Your task is to implement the Queue class with
+# two basic operations:
+
+# put(element), which puts an element at end of
+# the queue;
+
+# get(), which takes an element from the front of
+# the queue and returns it as the result (the
+# queue cannot be empty to successfully perform
+# it.)
+
+# Expected output
+# 1
+# dog
+# False
+# Queue error
+
+class QueueError(Exception): # Choose base class for the new exception.
+    # No need to explicitly invoke the Exception
+    # constructor unless adding custom attributes
+    # or behavior to the exception. 
+    pass
 
 class Queue:
     def __init__(self):
-        #
-        # Write code here
-        #
+        # use a list as your storage
+        self.__queue_list = []
 
     def put(self, elem):
-        #
-        # Write code here
-        #
+        # put() should append elements to the
+        # beginning of the list
+        self.__queue_list.insert(0,elem)
 
     def get(self):
-        #
-        # Write code here
-        #
-
+        # get() should remove the elements from
+        # the end of the list
+        if not self.__queue_list:
+            raise QueueError
+        return self.__queue_list.pop()
 
 que = Queue()
 que.put(1)
@@ -28,5 +60,5 @@ que.put(False)
 try:
     for i in range(4):
         print(que.get())
-except:
+except QueueError:
     print("Queue error")
