61d8197f6977ed702523253a3c3bda410e15c679
ysgorin <ysgorin@gmail.com>
2024-12-13 11:30:10 -0500

    completed code

--- python_exam_2_prep/third_module/weeker.py
+++ python_exam_2_prep/third_module/weeker.py
@@ -1,60 +1,31 @@
 # weeker.py
 
-# The class constructor accepts one argument – a
-# string. The string represents the name of the 
-# day of the week and the only acceptable values
-# must come from the following set:
-# Mon Tue Wed Thu Fri Sat Sun
-
-# Invoking the constructor with an argument from
-# outside this set should raise the WeekDayError
-# exception (define it yourself; don't worry,
-# we'll talk about the objective nature of
-# exceptions soon). The class should provide the
-# following facilities:
-
-# objects of the class should be "printable",
-# i.e. they should be able to implicitly convert
-# themselves into strings of the same form as the
-# constructor arguments;
-# the class should be equipped with one-parameter
-# methods called add_days(n) and
-# subtract_days(n), with n being an integer
-# number and updating the day of week stored
-# inside the object in the way reflecting the
-# change of date by the indicated number of days,
-# forward or backward.
-# all object's properties should be private;
-
 class WeekDayError(Exception):
     pass
 	
 
 class Weeker:
-    #
-    # Write code here.
-    #
+    __days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
 
+    # The class constructor accepts one argument – a
+    # string. The string represents the name of the 
+    # day of the week and the only acceptable values
+    # must come from the following set:
+    # Mon Tue Wed Thu Fri Sat Sun
     def __init__(self, day):
-        #
-        # Write code here.
-        #
+        if day not in Weeker.__days:
+            raise WeekDayError
+        self.__day_index = Weeker.__days.index(day)
 
     def __str__(self):
-        #
-        # Write code here.
-        #
+        return Weeker.__days[self.__day_index]
 
     def add_days(self, n):
-        #
-        # Write code here.
-        #
+        self.__day_index = (self.__day_index + n) % 7
 
     def subtract_days(self, n):
-        #
-        # Write code here.
-        #
-
+        # Reuse add_days code and turn n into a negative number
+        self.add_days(-n)
 
 try:
     weekday = Weeker('Mon')
