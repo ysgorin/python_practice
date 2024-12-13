130668d53e43ae6f9a4a5a56d0987a8838547853
ysgorin <ysgorin@gmail.com>
2024-12-13 10:35:27 -0500

    added commented instructions

--- python_exam_2_prep/third_module/weeker.py
+++ python_exam_2_prep/third_module/weeker.py
@@ -1,3 +1,31 @@
+# weeker.py
+
+# The class constructor accepts one argument – a
+# string. The string represents the name of the 
+# day of the week and the only acceptable values
+# must come from the following set:
+# Mon Tue Wed Thu Fri Sat Sun
+
+# Invoking the constructor with an argument from
+# outside this set should raise the WeekDayError
+# exception (define it yourself; don't worry,
+# we'll talk about the objective nature of
+# exceptions soon). The class should provide the
+# following facilities:
+
+# objects of the class should be "printable",
+# i.e. they should be able to implicitly convert
+# themselves into strings of the same form as the
+# constructor arguments;
+# the class should be equipped with one-parameter
+# methods called add_days(n) and
+# subtract_days(n), with n being an integer
+# number and updating the day of week stored
+# inside the object in the way reflecting the
+# change of date by the indicated number of days,
+# forward or backward.
+# all object's properties should be private;
+
 class WeekDayError(Exception):
     pass
 	
