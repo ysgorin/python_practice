701dfe9a566a6edf494b60d3b3c2fa82ffe5d76c
ysgorin <ysgorin@gmail.com>
2024-12-14 21:18:50 -0500

    added starter code

---
+++ python_exam_2_prep/third_module/point.py
@@ -0,0 +1,32 @@
+import math
+
+class Point:
+    def __init__(self, x=0.0, y=0.0):
+        #
+        # Write code here
+        #
+
+    def getx(self):
+        #
+        # Write code here
+        #
+
+    def gety(self):
+        #
+        # Write code here
+        #
+
+    def distance_from_xy(self, x, y):
+        #
+        # Write code here
+        #
+
+    def distance_from_point(self, point):
+        #
+        # Write code here
+        #
+
+point1 = Point(0, 0)
+point2 = Point(1, 1)
+print(point1.distance_from_point(point2))
+print(point2.distance_from_xy(2, 0))
