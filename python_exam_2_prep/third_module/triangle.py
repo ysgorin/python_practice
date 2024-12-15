5a542d75660d3df96536bf7937a9ac3e59f2b446
ysgorin <ysgorin@gmail.com>
2024-12-14 22:32:59 -0500

    changed sum to perimeter

--- python_exam_2_prep/third_module/triangle.py
+++ python_exam_2_prep/third_module/triangle.py
@@ -7,10 +7,10 @@ class Triangle:
         self.__vertices = [vertice1, vertice2, vertice3]
 
     def perimeter(self):
-        sum = 0
+        perimeter = 0
         for i in range(3):
-            sum += self.__vertices[i].distance_from_point(self.__vertices[(i + 1) % 3])
-        return sum
+            perimeter += self.__vertices[i].distance_from_point(self.__vertices[(i + 1) % 3])
+        return perimeter
 
 triangle = Triangle(Point(0, 0), Point(1, 0), Point(0, 1))
 print(triangle.perimeter())
