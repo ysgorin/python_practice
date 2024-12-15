bc8e392025ccbf678b6db0cf753fe05a0fc9865b
ysgorin <ysgorin@gmail.com>
2024-12-14 22:30:26 -0500

    simplified code using for loop

--- python_exam_2_prep/third_module/triangle.py
+++ python_exam_2_prep/third_module/triangle.py
@@ -7,7 +7,10 @@ class Triangle:
         self.__vertices = [vertice1, vertice2, vertice3]
 
     def perimeter(self):
-        return self.__points_list[0].distance_from_point(self.__points_list[1]) + self.__points_list[0].distance_from_point(self.__points_list[2]) + self.__points_list[1].distance_from_point(self.__points_list[2])
+        sum = 0
+        for i in range(3):
+            sum += self.__vertices[i].distance_from_point(self.__vertices[(i + 1) % 3])
+        return sum
 
 triangle = Triangle(Point(0, 0), Point(1, 0), Point(0, 1))
 print(triangle.perimeter())
