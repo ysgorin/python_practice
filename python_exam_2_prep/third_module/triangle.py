034f5dcee71af0cddaab12f68416551536b683fa
ysgorin <ysgorin@gmail.com>
2024-12-14 22:10:08 -0500

    added starter code

---
+++ python_exam_2_prep/third_module/triangle.py
@@ -0,0 +1,22 @@
+# triangle.py
+
+import math
+
+class Point:
+    #
+    # The code copied from the previous lab.
+    #
+
+class Triangle:
+    def __init__(self, vertice1, vertice2, vertice3):
+        #
+        # Write code here
+        #
+
+    def perimeter(self):
+        #
+        # Write code here
+        #
+
+triangle = Triangle(Point(0, 0), Point(1, 0), Point(0, 1))
+print(triangle.perimeter())
