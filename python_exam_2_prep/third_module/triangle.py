2ef95c710cbe88d919e07aeff3e5ea49d7c439cf
ysgorin <ysgorin@gmail.com>
2024-12-14 22:35:27 -0500

    changed perimeter variable to result

--- python_exam_2_prep/third_module/triangle.py
+++ python_exam_2_prep/third_module/triangle.py
@@ -7,10 +7,10 @@ class Triangle:
         self.__vertices = [vertice1, vertice2, vertice3]
 
     def perimeter(self):
-        perimeter = 0
+        result = 0
         for i in range(3):
-            perimeter += self.__vertices[i].distance_from_point(self.__vertices[(i + 1) % 3])
-        return perimeter
+            result += self.__vertices[i].distance_from_point(self.__vertices[(i + 1) % 3])
+        return result
 
 triangle = Triangle(Point(0, 0), Point(1, 0), Point(0, 1))
 print(triangle.perimeter())
