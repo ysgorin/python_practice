ea51a179c3f14267eaf25510e906eea677de0366
ysgorin <ysgorin@gmail.com>
2025-02-19 11:08:39 -0500

    added second version to simple_write.py

--- python_exam_2_prep/fourth_module/simple_write.py
+++ python_exam_2_prep/fourth_module/simple_write.py
@@ -4,8 +4,15 @@ try:
 	fo = open('newtext.txt', 'wt') # A new file (newtext.txt) is created.
 	for i in range(10):
 		s = "line #" + str(i+1) + "\n" # Note: the newline is added
-		for ch in s:
-			fo.write(ch)
+		# Version 1
+		# The following loops through the string
+		# and writes one character at a time.
+		# for ch in s:
+		# 	fo.write(ch)
+
+		# Version 2
+		# The following writes the entire string
+		fo.write(s)
 	fo.close()
 except IOError as e:
 	print("I/O error occurred: ", strerror(e.errno))
