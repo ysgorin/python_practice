121b26d1ff00414673b372e2b487eb22e94b8df7
ysgorin <ysgorin@gmail.com>
2025-02-24 09:41:51 -0500

    added write_byte.py

---
+++ python_exam_2_prep/fourth_module/write_byte.py
@@ -0,0 +1,14 @@
+# import strerror for diagnosing stream problems
+from os import strerror
+
+data = bytearray(10)
+
+for i in range(len(data)):
+    data[i] = 10 + i
+
+try:
+    bf = open('file.bin', 'wb')
+    bf.write(data)
+    bf.close()
+except IOError as e:
+    print("I/O error occured:", strerror(e.errno))
