49bd283d1b4bceef61b2e737d7b9b92aeeb114c6
ysgorin <ysgorin@gmail.com>
2025-02-24 10:23:39 -0500

    added char_freq_histo.py

---
+++ python_exam_2_prep/fourth_module/char_freq_histo.py
@@ -0,0 +1,12 @@
+# char_freq_histo.py
+
+# Character Frequency Histogram
+
+# Write a program that asks that
+# - asks the user for the input file's name;
+# - reads the file and counts all the Latin
+# letters (lower- and upper-case letters are
+# treated as equal)
+# - prints a simple histogram in alphabetical
+# order (only non-zero counts should be
+# presented)
