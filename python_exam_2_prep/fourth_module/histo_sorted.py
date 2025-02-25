918e0e668c074133e0c9a0cd4ae4870b0437d820
ysgorin <ysgorin@gmail.com>
2025-02-25 08:48:57 -0500

    added histo_sorted.py copied code from char_freq_histo.py

---
+++ python_exam_2_prep/fourth_module/histo_sorted.py
@@ -0,0 +1,41 @@
+# histo_sorted.py
+
+# Sorted character frequency histogram
+
+# Expected output based on test_file.txt:
+# a -> 3
+# b -> 2
+# c -> 1
+
+from os import strerror
+
+# ask the user for the input file's name 
+file_name = input('Enter file name: ')
+histo_dict = {}
+
+try:
+    # read the file and count all the Latin
+    # letters (lower- and upper-case letters are
+    # treated as equal)
+    s = open(file_name, 'r')
+    ch = s.read(1)
+    while ch != '':
+        # if it is a Latin character
+        if 65 <= ord(ch) <= 90 or 97 <= ord(ch) <= 122:
+            if ch.lower() in histo_dict:
+                histo_dict[ch.lower()] += 1
+            else:
+                histo_dict[ch.lower()] = 1
+        ch = s.read(1)
+    s.close()
+except IOError as e:
+    print('I/O error occurred: ', strerror(e.errno))
+except:
+    print('A non-I/O error occured')
+
+# prints a simple histogram in alphabetical
+# order (only non-zero counts should be
+# presented)
+for k, v in sorted(histo_dict.items()):
+    if v > 0:
+        print(k, '->', v)
