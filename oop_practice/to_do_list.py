1dd600b101a7da0d5304c6b42902a83febb285e3
ysgorin <ysgorin@gmail.com>
2025-03-07 09:33:23 -0500

    added comments and shell

---
+++ oop_practice/to_do_list.py
@@ -0,0 +1,15 @@
+# to_do_list.py
+
+# To-Do List Manager
+# * Define a Task class with attributes
+#   description, due_date, and completed.
+# * Implement methods to mark_complete(),
+#   edit_task(), and show_task_details().
+# * Store multiple tasks in a ToDoList class and
+#   allow searching and sorting.
+
+class Task:
+    pass
+
+class ToDoList:
+    pass
