e253733dfa22f31f388981992c82b7fd14401b58
ysgorin <ysgorin@gmail.com>
2025-03-10 11:03:57 -0400

    added commented instructions

---
+++ python_exam_2_prep/fourth_module/datetime_ex.py
@@ -0,0 +1,17 @@
+# Write a program that creates a datetime object
+# for November 4, 2020, 14:53:00. The object
+# created should call the strftime method with
+# the appropriate format to display the following
+# result:
+
+# 2020/11/04 14:53:00
+# 20/November/04 14:53:00 PM
+# Wed, 2020 Nov 04
+# Wednesday, 2020 November 04
+# Weekday: 3
+# Day of the year: 309
+# Week number of the year: 44
+
+# Note: Each result line should be created by
+# calling the strftime method with at least one
+# directive in the format argument.
