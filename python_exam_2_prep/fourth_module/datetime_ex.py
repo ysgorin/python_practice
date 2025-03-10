276ed850c2bbebef6f8fc09d2ca51a7223b3107a
ysgorin <ysgorin@gmail.com>
2025-03-10 11:15:10 -0400

    finished code

--- python_exam_2_prep/fourth_module/datetime_ex.py
+++ python_exam_2_prep/fourth_module/datetime_ex.py
@@ -15,3 +15,16 @@
 # Note: Each result line should be created by
 # calling the strftime method with at least one
 # directive in the format argument.
+
+# Dependency
+from datetime import datetime
+
+dt = datetime(2020, 11, 4, 14, 53, 0)
+
+print(dt.strftime('%Y/%m/%d %H:%M:%S'))
+print(dt.strftime('%y/%B/%d %H:%M:%S %p'))
+print(dt.strftime('%a, %Y %b %d'))
+print(dt.strftime('%A, %Y %B %d'))
+print('Weekday:', dt.strftime('%w'))
+print('Day of the year:', dt.strftime('%j'))
+print('Week number of the year:', dt.strftime('%W'))
