c32eebbbfc39d0a83be9494f5196ec807760f1ba
ysgorin <ysgorin@gmail.com>
2025-03-13 08:47:18 -0400

    finished code

--- python_exam_2_prep/fourth_module/weekday_count.py
+++ python_exam_2_prep/fourth_module/weekday_count.py
@@ -14,11 +14,22 @@
 # Create a class called MyCalendar that extends
 # the Calendar class;
 
+import calendar
+
+class MyCalendar(calendar.Calendar):
+
 # create the count_weekday_in_year method with
 # the year and weekday parameters. The weekday
 # parameter should be a value between 0-6, where
 # 0 is Monday and 6 is Sunday. The method should
 # return the number of days as an integer;
+    def count_weekday_in_year(self, year, weekday):
+        weekday_count = 0
+        for month in range(1,13):
+            for i in self.monthdays2calendar(year, month):
+                if i[weekday][0] != 0:
+                    weekday_count += 1
+        return weekday_count
 
 # in your implementation, use the
 # monthdays2calendar method of the Calendar class.
@@ -33,3 +44,10 @@
 # year=2000, weekday=6
 # Expected output
 # 53
+
+c = MyCalendar()
+
+year = int(input('Enter a year: '))
+weekday = int(input('Enter a weekday integer (0-6): '))
+
+print(c.count_weekday_in_year(year, weekday))
