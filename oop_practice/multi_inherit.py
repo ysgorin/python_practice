007ad3b7e29ea557f5b816eac564485819b91bc3
ysgorin <ysgorin@gmail.com>
2025-03-31 10:38:06 -0400

    updated code

--- oop_practice/multi_inherit.py
+++ oop_practice/multi_inherit.py
@@ -1,18 +1,18 @@
 #  multi_inherit.py
 
 class Scanner:
-    def scan():
+    def scan(self):
         print('scan() method from the Scanner class.')
 
 class Printer:
-    def print():
+    def print(self):
         print('print() method from the Printer class.')
 
 class Fax:
-    def send():
+    def send(self):
         print('send() method from the Fax class.')
 
-    def print():
+    def print(self):
         print('print() method from the Fax class.')
 
 class MFD_SPF(Scanner, Printer, Fax):
